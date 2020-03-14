3f736592ac081fa5353586ba19ce84e023e3bb33
Jeremy <JBCripe@gmail.com>
2020-03-14 18:51:27 -0500

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -1,6 +1,24 @@
-console.log("Up and running!");
-
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"	}
+];
 
 let cardsInPlay = [];
 
@@ -9,18 +27,21 @@ function checkForMatch(){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 		} else {
-		console.log("Sorry, try again.")
+		alert("Sorry, try again.")
 		}
 	}
 }
 
 function flipCard(cardID){
 
-	console.log("User flipped " + cards[cardID])
+	console.log("User flipped " + cards[cardID].rank)
 
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
 
 	checkForMatch()
+
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 
 }
 
